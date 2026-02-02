1ac646db3cedbe13b4c9de35c00ef21d94217217
RaphyStoll <raphael.ferreira@ik.me>
2026-02-02 14:58:06 +0100

    ADD: implementation de la classe reponse builder

---
+++ src/core/ResponseBuilder/helper/get_status_message.cpp
@@ -0,0 +1,23 @@
+#include <string>
+
+#include "../../../../include/ResponseBuilder.hpp"
+
+std::string webserv::http::ResponseBuilder::getStatusMessage(int code) {
+    switch (code) {
+        case 200: return "OK";
+        case 201: return "Created";
+        case 204: return "No Content";
+        case 301: return "Moved Permanently";
+        case 302: return "Found";
+        case 400: return "Bad Request";
+        case 403: return "Forbidden";
+        case 404: return "Not Found";
+        case 405: return "Method Not Allowed";
+        case 413: return "Payload Too Large";
+        case 500: return "Internal Server Error";
+        case 501: return "Not Implemented";
+        case 502: return "Bad Gateway";
+        case 504: return "Gateway Timeout";
+        default:  return "Unknown Error";
+    }
+}
